61fd5cfdc6a78ad4ddceb60fd93f14797b389619
RalucaM13 <157896258+RalucaM13@users.noreply.github.com>
2024-02-25 19:30:59 +0000

    final changes

--- src/script.js
+++ src/script.js
@@ -17,13 +17,14 @@ function generatePoem(event) {
 
   let apiKey = "b13fe4d2a8e30dd1a60bbd2oaf1tb1be";
   let context =
-    "You are a poetry expert and love to write poems in the style of Alphonse de Lamartine, Edgar Allan Poe and Charles Baudelaire. Your mission is to generate a 4 verse poem, each verse on a new line, starts with capital letter. The last line will have an ellipsis at the end, followed by this emoji 🪻. Do not include a title. Make sure the poem rhymes. Make sure to follow the user instructions. ";
-  let prompt = `User instructions: Generate a quatrain poem about ${instructionsInput.value}`;
+    "You are a poetry expert and love to write short poems in the style of Alphonse de Lamartine, Edgar Allan Poe and Charles Baudelaire. Your mission is to generate a 4 verse poem, each verse on a new line, starts with capital letter. The last line will have an ellipsis at the end, followed by this emoji 🪻. Do not include a title. Make sure the poem rhymes. Make sure to follow the user instructions. ";
+  let prompt = `User instructions: Generate a 4 verse poem about ${instructionsInput.value}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating your poem about ${instructionsInput.value}</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 
